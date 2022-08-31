26a3f086f5c78b3a4086d9fa99c15c3b75990941
tujinfei123 <tjf2585730694@163.com>
2022-08-31 17:37:16 +0800

    tijiao

--- components/hamburger-popup/hamburger-popup.js
+++ components/hamburger-popup/hamburger-popup.js
@@ -1,7 +1,29 @@
 Component({
   mixins: [],
   data: {
-    basicShow: false
+    basicShow: false,
+    dataOrigin: {
+      xiaoxi: [
+        { icon: "", text: "我的消息" },
+        { icon: "", text: "云贝中心" },
+        { icon: "", text: "创作者中心" }
+      ],
+      音乐: [
+        { icon: "", text: "云村" },
+        { icon: "", text: "商" },
+        { icon: "", text: "多多" }
+      ],
+      其他: [
+        { icon: "", text: "设" },
+        { icon: "", text: "深色", hasArrow: false },
+        { icon: "", text: "定时" }
+      ],
+      dingdan: [
+        { icon: "", text: "我的订单" },
+        { icon: "", text: "优惠" },
+        { icon: "", text: "客服" }
+      ]
+    }
   },
   props: {},
   didMount() {},
